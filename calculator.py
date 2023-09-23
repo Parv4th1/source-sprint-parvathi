387bd988f9f032de96975652c41c9d13ee6dc01c
Parv4th1 <126835050+Parv4th1@users.noreply.github.com>
2023-09-23 14:52:58 +0530

    Feature added multiplication.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@ X = eval(input("Enter the first number: "))
 Y = eval(input("Enter the second nuber: "))
 total = X+Y
 diff = X-Y
+prod = X*Y
 print(total)
 print(diff)
-print("This is a calculator")
+print(prod)
